8ffc7e8ec5223495366c401b15f0474956160d8b
tejasware <tejas.22010447@viit.ac.in>
2021-07-06 22:18:04 +0530

    Added fetchdetails function

--- db_manager.py
+++ db_manager.py
@@ -14,6 +14,22 @@ def insertPassword(appName,encPass,key,userEmail,userName,url):
     except:
         print("Error")
 
+def fetchDetails(userEmail):
+    try:
+        dbPass = getpass("Enter Password for connecting to the database: ")
+        db = sqltor.connect(host="localhost", user="root", password=dbPass, database='megapass')
+        cursor = db.cursor()
+        selectQuery = "SElECT appName,userName,url from accounts WHERE userEmail = '{}'".format(userEmail)
+        cursor.execute(selectQuery)
+        data = cursor.fetchall()
+        print("USER EMAIL: ",userEmail)
+        for row in data:
+            print(("APPNAME: "+row[0],"USERNAME: "+row[1],"URL: "+row[2]))
+        print("-"*30)
+        db.close()
+        
+    except:
+        print("Error")
 
 def findPassword(appName):
     try:
